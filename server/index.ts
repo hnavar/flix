4f81c063776496b1a8fdd14eafe1b6e4132d15d6
hnavar <83598123+hnavar@users.noreply.github.com>
2021-12-14 12:22:49 -0600

    express server setup

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,16 @@
 const path = require ('path');
 const express = require('express');
-const app = express();
 const cors = require('cors');
-// const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const port = process.env.PORT || 3000;
+const app = express();
 
 const dist = path.resolve(__dirname, 'client', 'dist');
 app.use(cors({origin: true, credentials: true}));
-
+app.use(express.static(dist));
+app.use(bodyParser.json());
 
 
 
