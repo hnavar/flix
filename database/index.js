09c22f25cd0b46392e8f33e5904eca4e6b7b4626
sbelete01 <sbelete01@gmail.com>
2021-12-14 23:39:04 -0800

    update: created all schemas and tables for db moved postgres setup to .env

--- database/index.js
+++ database/index.js
@@ -1,18 +1,20 @@
-const user = '<postgres user>'
-const host = 'localhost'
-const database = '<postgres db name>'
-const password = '<postgres password>'
-const port = '<postgres port>'
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-import { Sequelize, Model, DataTypes } from 'sequelize'
-// require(sequelize)
+const {
+    DATABASE,
+    USER_NAME,
+    USER_PASSWORD,
+    HOST,
+    PORT,
+} = process.env;
 
 const db = new Sequelize({
-    database: 'flick_db',
-    username: 'samuelbelete',
-    password: '',
-    host: 'localhost',
-    port: 5432,
+    database: DATABASE,
+    username: USER_NAME,
+    password: USER_PASSWORD,
+    host: HOST,
+    port: PORT,
     dialect: 'postgres',
     logging: false,
   })
@@ -48,11 +50,106 @@ const Genre = db.define('genre', {
     genre: Sequelize.STRING,
 });
 
-// User.belongsToMany(Profile, { through: 'User_Profiles' });
-// Profile.belongsToMany(User, { through: 'User_Profiles' });
+const Movies = db.define('movies', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    title: Sequelize.STRING,
+    trailer_url: Sequelize.STRING,
+     description: Sequelize.STRING,
+    cast: Sequelize.STRING,
+    release_date: Sequelize.STRING
+});
 
+const Actors = db.define('actors', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    actor_name: Sequelize.STRING
+});
 
+const Directors = db.define('directors', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    director_name: Sequelize.STRING
+});
 
+// join tables
+const Users_Genre = db.define('users_genre', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    }
+});
+
+const Users_Movies = db.define('users_movies', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    }
+});
+
+const Movie_Genre = db.define('movie_genre', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    }
+});
+
+const Movie_Actors = db.define('movie_actors', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    }
+});
+
+const Movie_Directors = db.define('movie_directors', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    }
+});
+
+// creates userId & genreId collumns in users_genre table
+User.belongsToMany(Genre, { through: 'users_genre' });
+Genre.belongsToMany(User, { through: 'users_genre' });
+
+// creates userId & movieId collumns in users_movies table
+User.belongsToMany(Movies, { through: 'users_movies' });
+Movies.belongsToMany(User, { through: 'users_movies' });
+
+// creates movieId & genreId collumns in movie_genre table
+Movies.belongsToMany(Genre, { through: 'movie_genre' });
+Genre.belongsToMany(Movies, { through: 'movie_genre' });
+// small bug creates table as movie_genres
+
+// creates movieId & actorsId collumns in movie_actors table
+Movies.belongsToMany(Actors, { through: 'movie_actors' });
+Actors.belongsToMany(Movies, { through: 'movie_actors' });
+
+// creates movieId & directorsID collumns in movie_actors table
+Movies.belongsToMany(Directors, { through: 'movie_directors' });
+Directors.belongsToMany(Movies, { through: 'movie_directors' });
 
 User.sync();
 Genre.sync();
+Movies.sync();
+Actors.sync();
+Directors.sync();
+Users_Genre.sync(); 
+Users_Movies.sync();
+Movie_Genre.sync();
+Movie_Actors.sync();
+Movie_Directors.sync();
