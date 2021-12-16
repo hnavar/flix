b6a1639e5437722a9fbe3d707311d40207f7d0ec
Ben Adams <badams92@gmail.com>
2021-12-15 19:45:50 -0600

    moved database/index to the server folder

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ const path = require ('path');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const {MoviesRouter} = require('./api/movies')
 // const dotenv = require('dotenv');
 
 const port = process.env.PORT || 3000;
@@ -10,6 +11,7 @@ const dist = path.resolve(__dirname, '..', 'client/dist');
 app.use(cors({origin: true, credentials: true}));
 app.use(express.static(dist))
 
+app.use('/api/movies', MoviesRouter);
 
 
 
