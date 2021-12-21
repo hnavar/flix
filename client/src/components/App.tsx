ba63bc7bee272eee174ac3bd37f25c5ff528f22e
Ben Adams <badams92@gmail.com>
2021-12-21 15:28:21 -0600

    started work on drawer

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -2,11 +2,13 @@ import React, { FC } from 'react';
 import SearchMovie from './movieSearch';
 import Home from './Home';
 import Twitter from './Twitter';
+import TemporaryDrawer from './NavDrawer';
 
 const App:FC = () => {
 
   return (
     <div className ='App'>
+      <TemporaryDrawer />
       <SearchMovie/>
       <Home />
       {/* <Home /> */}
