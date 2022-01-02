2c0a5aa479ab722607dab5fa9726d5150367b44e
sbelete01 <sbelete01@gmail.com>
2022-01-02 15:03:55 -0800

    Added Discover component to front end

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -2,6 +2,7 @@ import Home from "./components/Home";
 import SearchMovie from "./components/movieSearch";
 import Twitter from "./components/Twitter";
 import Profile from './components/Profile';
+import Discover from "./components/Discover";
 
 
 const Paths = [
@@ -19,6 +20,11 @@ const Paths = [
     path: 'twitter',
     sidebarName: 'Twitter',
     component: Twitter
+  },
+  {
+    path: '/discover',
+    sidebarName: 'Discover',
+    component: Discover
   },
   {
     path: '/profile',
