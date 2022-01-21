12c1d1e8a95cef0023c3a9bd9e8666a0bd635c5f
Ben Lyon <carlbenjaminlyon@gmail.com>
2022-01-20 18:01:02 -0600

    working on login button styling

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState, FC } from 'react';
 import { Button, Box, Grid, Card, TextField, Paper, Typography, Avatar, Link, CardMedia } from '@material-ui/core';
 import TsParticles from './tsParticle/tsParticles';
 import axios from 'axios';
-// import flixar from '../images/flixar.png';
-// let img = '../images/flixar.png';
-// const flixar = require('../images/flixar.png');
+
+//MUI
+import GoogleIcon from '@mui/icons-material/Google';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
 
 export const Login:FC<any> = ({user}) => {
   const [currentUser, setCurrentUser] = useState<any>();
@@ -21,23 +23,54 @@ export const Login:FC<any> = ({user}) => {
     })
   }, [user]);
 
-  // const Content = () => {
-  //   // const image = URL.createObjectURL(img);
-  //   return (
-  //   <div>
-  //     <img src={flixar} />
-  //   </div>
-  //   );
-  // }
+
   // https://res.cloudinary.com/doruu9b3f/image/upload/v1641818928/snap_flixar_ngn5ui.png
-  // console.log(flixar);
+
+  const paperStyle = {padding: 20, height: 450, width: 280, margin:'20px auto'};
+  const avatarStyle = {width: 200, height: 200, opacity: 1.0};
   return (
   <>
   <div>
     <TsParticles />
     {!currentUser ?
     <>
-        <div
+    <Grid>
+      <Paper elevation={10} style={paperStyle}>
+        <Grid container
+              direction='column'
+              justifyContent='center'
+              alignItems='center'
+              spacing={4}
+        >
+          <Avatar style={avatarStyle}
+                  src='https://res.cloudinary.com/doruu9b3f/image/upload/v1641818928/snap_flixar_ngn5ui.png'/>
+          <Typography variant='h6'>Sign in</Typography>
+        <TextField label='Username *' placeholder='Enter username' />
+        <TextField label='Password *' placeholder='Enter password' type='password' />
+        <Grid container
+              direction='row'
+              justifyContent='space-evenly'
+              alignItems='center'
+        >
+        <Button type='submit' color='primary' variant='contained'>Sign In!</Button>
+        <Button type='submit' color='primary' variant='contained'>Sign Up!</Button>
+        </Grid>
+        <Typography variant='caption'>Sign in with:</Typography>
+        <Grid container
+              direction='row'
+              justifyContent='space-evenly'
+              alignItems='flex-end'
+        >
+          <Button type='submit' color='primary' variant='contained' startIcon={<GoogleIcon />} href='/auth/google'>Google</Button>
+          <Button type='submit' color='primary' variant='contained' startIcon={<TwitterIcon />}>Twitter</Button>
+        </Grid>
+        </Grid>
+      </Paper>
+    </Grid>
+
+
+
+        {/* <div
           style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '50vh'}}
         >
             <Button variant='contained' color='inherit'
@@ -47,7 +80,7 @@ export const Login:FC<any> = ({user}) => {
         </Button>
             <img src='https://res.cloudinary.com/doruu9b3f/image/upload/v1641818928/snap_flixar_ngn5ui.png' alt="flixar" />
 
-        </div>
+        </div> */}
     </>
     :
     <>
